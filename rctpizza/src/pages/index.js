170105178b7c362298bb8dbd92ff8b8f9817e998
Nick Chalkov <53650602+chlkvnck@users.noreply.github.com>
2021-06-29 17:01:48 +0300

    Delete rctpizza directory

--- rctpizza/src/pages/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Home } from '../pages/Home';
-export { default as Cart } from '../pages/Cart';
--- rctpizza/src/redux/actions/filters.js
+++
@@ -1,9 +0,0 @@
-export const setSortBy = (name) => ({
-  type: 'SET_SORT_BY',
-  payload: name
-});
-
-export const setCategory = (indexCategory) => ({
-  type: 'SET_CATEGORY',
-  payload: indexCategory
-});
